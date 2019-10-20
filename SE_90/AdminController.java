dd3763d03466ac220916f32f735010503cd58076
Yuekai Huang <yhua0086@student.monash.edu>
2019-10-20 22:24:40 +1100

    read write admin

--- SE_90/AdminController.java
+++ SE_90/AdminController.java
@@ -1,3 +1,5 @@
+import java.io.*;
+import java.util.Scanner;
 
 /**
  * Class ListOfAdmins
@@ -13,9 +15,13 @@ public class AdminController
      * 
      */
     public AdminController()
-    {
-        admin = new Administrator[1];
-        admin[0] = new Administrator("admin@gmail.com", "admin");
+    {        
+        int totalNumAdmin = 99999;
+        admin = new Administrator[totalNumAdmin];
+        for(int index = 0; index < totalNumAdmin; index++)
+        {
+            admin[index] = new Administrator("????","????");
+        }
     }
     
     /**
@@ -32,5 +38,68 @@ public class AdminController
     public void setAdmin(Administrator[] admin)
     {
         this.admin = admin;
+    }
+    
+    public Administrator[] getAllAdmin()
+    {
+        return admin;
+    }
+    
+    public void setAdmin(int index, String account, String password)
+    {
+        admin[index] = new Administrator(account, password);
+    }
+    
+    public void writeAdmin(String fileName)
+    {
+        String message = "";
+        String output = "";
+        try
+        {
+            PrintWriter outputFile = new PrintWriter(fileName);
+            for(int index = 0; index < getAllAdmin().length; index++)
+            {
+                if(getAdmin(index).getAdminEmail().equals("????"))
+                    break;
+                message = getAdmin(index).getAdminEmail() + "," + getAdmin(index).getAdminPassword()+";";
+                output = output + message;
+            }
+            outputFile.println(output);
+            outputFile.close();
+        }
+        catch(FileNotFoundException exception)
+        {
+            System.out.println(fileName + " not found");
+        }
+        catch(IOException exception)
+        {
+            System.out.println("Unexpected I/O error occured");
+        }
+    }
+    
+    public void readAdmin(String fileName)
+    {
+        try
+        {
+            FileReader inputFile = new FileReader(fileName);
+            Scanner parser = new Scanner(inputFile);
+            String[] array = parser.nextLine().split(";");
+            for(int index = 0; index < array.length; index++)
+            {
+                String[] elements = array[index].split(",");
+                String account = elements[0];
+                String password = elements[1];
+                setAdmin(index, account, password);
+            }
+        }
+        catch(FileNotFoundException exception)
+        {
+            System.out.println(fileName + " not found");
+        }
+        catch(IOException exception)
+        {
+            System.out.println("Unexpected I/O error occured");
+        }
+
     }
 }
